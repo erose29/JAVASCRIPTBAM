bf2d796ac0898671bb2d066068a33e39881a6e55
ROSA CARBALLO <115595304+erose29@users.noreply.github.com>
2022-11-21 22:22:59 -0300

    Se agregaron ejercicios de JS

---
+++ Class02.js
@@ -0,0 +1,14 @@
+//Ingreso de datos por el usuario
+
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let numero2 = parseInt(prompt("Ingrese un numero"));
+
+alert("El resultado de tu operacion es:" + (numero1 + numero2));
+
+let boleano = false
+
+if (boleano){
+    console.log ("Entre en la seccion True")
+}else{
+    console.log ("Entre en la seccion False")
+}
